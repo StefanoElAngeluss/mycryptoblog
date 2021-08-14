40e6f3c805b20d3e7af532efefa5cdd91c2c5624
Stefano <jesty2002@yahoo.fr>
2021-08-14 08:50:43 +0200

    rectification webhooks

--- app/controllers/webhooks_products_controller.rb
+++ app/controllers/webhooks_products_controller.rb
@@ -8,7 +8,7 @@ class WebhooksProductsController < ApplicationController
 
     begin
       event = Stripe::Webhook.construct_event(
-        payload, sig_header, Rails.application.credentials[:stripe][:webhooks]
+        payload, sig_header, Rails.application.credentials[:stripe][:webhook]
       )
     rescue JSON::ParserError => e
       status 400
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # Checkout Product
   post "checkout/create", to: "checkout#create"
   # Webhooks
-  resources :webhooks, only: [:create]
+  resources :webhooks_products, only: [:create]
 
 end
