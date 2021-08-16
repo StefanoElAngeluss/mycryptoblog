4bf6da0fbf58936a8ac58d0ffd39825d8c662a03
Stefano <jesty2002@yahoo.fr>
2021-08-17 01:15:51 +0200

    session based shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :initialize_session
+  before_action :load_cart
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,6 +10,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :remember_me])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+  end
+
+  private
+
+  def initialize_session
+    session[:cart] ||= [] # empty cart = empty array
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
   end
 
 end
