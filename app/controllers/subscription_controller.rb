3f939356f5c4d20d7e416ec660627275e670931f
Stefano <jesty2002@yahoo.fr>
2021-08-19 23:55:06 +0200

    ajout de current_user pour le plan d'abonnement

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -2,11 +2,12 @@ class SubscriptionController < ApplicationController
 
   def create
     @session = Stripe::Checkout::Session.create({
+      customer: current_user.stripe_customer_id,
       success_url: posts_url,
       cancel_url: prix_url,
       payment_method_types: ['card'],
       line_items: [
-        {price: 'price_1JNGkzHRbsmqsi3fyrmOT9ii', quantity: 1},
+        {price: params[:prix], quantity: 1},
       ],
       mode: 'subscription',
     })
