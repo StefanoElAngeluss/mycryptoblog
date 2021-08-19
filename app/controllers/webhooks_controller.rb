977ddd89ac0fefba1f249c391c65bb52ca7f8952
Stefano <jesty2002@yahoo.fr>
2021-08-20 01:22:46 +0200

    modif webhook suite

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -30,13 +30,18 @@ class WebhooksController < ApplicationController
         product = Product.find_by(stripe_product_id: line_item.price.product)
         product.increment!(:sales_count)
       end
+    end
 
-      user = User.find_by(stripe_customer_id: session.customer)
-      user.update(subscription_status: 'active')
+    # Handle the event
+    case event.type
+    when 'checkout.session.completed'
+      session = event.data.object
+      @user = User.find_by(stripe_customer_id: session.customer)
+      @user.update(subscription_status: 'active')
     when 'customer.subscription.updated', 'customer.subscription.deleted'
       subscription = event.data.object
-      user = User.find_by(stripe_customer_id: subscription.customer)
-      user.update(
+      @user = User.find_by(stripe_customer_id: subscription.customer)
+      @user.update(
         subscription_status: subscription.status,
         plan: subscription.items.data[0].prix.lookup_key,
       )
