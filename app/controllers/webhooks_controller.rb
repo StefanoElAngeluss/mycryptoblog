a28883f28ff64347e1a421221564bc8a977a1d4e
Stefano <jesty2002@yahoo.fr>
2021-08-20 02:17:22 +0200

    modif webhook test 2

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -9,7 +9,7 @@ class WebhooksController < ApplicationController
 
     begin
       event = Stripe::Webhook.construct_event(
-        payload, sig_header, Rails.application.credentials.dig(:stripe, :webhook)
+        payload, sig_header, Rails.application.credentials[:stripe][:webhook]
       )
     rescue JSON::ParserError => e
       status 400
@@ -25,17 +25,18 @@ class WebhooksController < ApplicationController
     case event.type
     when 'checkout.session.completed'
       session = event.data.object
-      @user = User.find_by(stripe_customer_id: session.customer)
-      @user.update(subscription_status: 'active', plan: subscription.items.data[0].prix.lookup_key)
+      
 
       session_with_expand = Stripe::Checkout::Session.retrieve({ id: session.id, expand: ["line_items"]})
       session_with_expand.line_items.data.each do |line_item|
         product = Product.find_by(stripe_product_id: line_item.price.product)
         product.increment!(:sales_count)
       end
-      
-    when 'customer.subscription.updated', 'customer.subscription.deleted'
+
+    when 'customer.subscription.updated', 'customer.subscription.deleted', 'customer.subscription.created'
       subscription = event.data.object
+      @user = User.find_by(stripe_customer_id: session.customer)
+      @user.update(subscription_status: "active")
       @user = User.find_by(stripe_customer_id: subscription.customer)
       @user.update(
         subscription_status: subscription.status,
