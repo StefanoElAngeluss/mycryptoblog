34c5fcced155bd78cd94935cb9805d4633c09fe2
Stefano <jesty2002@yahoo.fr>
2021-08-20 11:13:06 +0200

    modif webhook test 4

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def prix
+    @prix = Stripe::Price.list(lookup_keys: ["medium_year", "basic_month"], expand: ["data.product"]).data.sort_by {|p| p.unit_amount}
   end
   
 end
--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -37,14 +37,13 @@ class WebhooksController < ApplicationController
     when 'customer.created'
       customer = event.data.object
       @user = User.find_by(email: customer.email)
-      @user.update(stripe_customer_id: customer.id)
+      @user.update(stripe_customer_id: customer.id, subscription_status: "active")
     when 'customer.subscription.updated', 'customer.subscription.deleted', 'customer.subscription.created'
       subscription = event.data.object
       @user = User.find_by(stripe_customer_id: subscription.customer)
       @user.update(
         subscription_status: subscription.status,
         plan: subscription.items.data[0].price.lookup_key,
-        current_period_end: Time.at(subscription.current_period_end).to_datetime,
       )
 
     end
